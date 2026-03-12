1bef3549840d3166dbf4d1193e1ad6686cdf8c0d
harivonyR* <harivonyratefiarison@gmail.com>
2026-03-12 21:15:45 +0300

    finishing code

--- language c/code et algo/7_string_inverter.c
+++ language c/code et algo/7_string_inverter.c
@@ -2,14 +2,19 @@
 ALGORITHM: un programme qui inverse une chaîne de caractères.
 ex : MIT -> TIM
 
+
 début
 
-    chaine_originale = "MIT"
-    chaine_inversée = chaine_originale
-
+    fonction count_string_length(chaine)
+        longueur = 0
+        tant que chaine[longueur] != '\0':
+            longueur = longueur + 1
+        retourner longueur
+    
     fonction reverse_string(chaine_originale)
         i=0
-        longueur = longueur de chaine_originale
+        longueur = count_string_length(chaine_originale)
+
         tant que i < longueur:
             chaine_inversée[i] = chaine_originale[longueur - 1 - i]
             i = i + 1
@@ -25,13 +30,28 @@ début
 #include <stdio.h>
 #include <string.h>
 
-
-void reverse_string(char str[], char reversed_str[]) {
-    int length = strlen(str);
-    
-    for (int i = 0; i < length; i++) {
-        reversed_str[i] = str[length - 1 - i];
+int count_string_length(char chaine[]) {
+    int longueur = 0;
+    while (chaine[longueur] != '\0') {
+        longueur++;
     }
-    
-    reversed_str[length] = '\0'; // Ajouter le caractère de fin de chaîne
+    return longueur;
+}
+
+void reverse_string(char original[], char reversed[]) {
+    int length = count_string_length(original);
+    for (int i = 0; i < length; i++) {
+        reversed[i] = original[length - 1 - i];
+    }
+    reversed[length] = '\0'; // Ajouter le caractère de fin de chaîne
+}
+
+int main() {
+    char original[] = "MIT";
+    int length = count_string_length(original);
+    char reversed[length + 1]; // ajouter une case pour le caractère de fin de chaîne '\0'
+    reverse_string(original, reversed);
+    printf("La chaîne inversée est : %s\n", reversed);
+
+    return 0;
 }
--- language c/code et algo/8_count_string_length.c
+++ language c/code et algo/8_count_string_length.c
@@ -1,1 +1,33 @@
+/*
+ALGORITHM : compter le nombre de caractères dans une chaîne de caractères.
+
+début
+    fonction count_string_length(chaine)
+        longueur = 0
+        tant que chaine[longueur] != '\0':
+            longueur = longueur + 1
+        retourner longueur
+
+    fonction main()
+        chaine = "Hello, World!"
+        résultat = count_string_length(chaine)
+        Afficher "La longueur de la chaîne est : ", résultat
+
+*/
+
 #include <stdio.h>
+
+int count_string_length(char chaine[]) {
+    int longueur = 0;
+    while (chaine[longueur] != '\0') {
+        longueur++;
+    }
+    return longueur;
+}
+
+int main() {
+    char chaine[] = "Hello, World!";
+    int result = count_string_length(chaine);
+    printf("La longueur de la chaîne est : %d\n", result);
+    return 0;
+}
